fd87110fb5e5de21a0f9cd58973f2dfecf990fd1
Dave-code-creater <dat981dat@gmail.com>
2025-11-15 17:11:46 -0500

    Add new routes and test case for create user

--- src/api/users_api.rs
+++ src/api/users_api.rs
@@ -1,6 +1,7 @@
-use actix_web::{HttpResponse, get, web};
+use actix_web::{HttpResponse, get, web, post};
 use crate::{AppState, models::user::UserResponse, services::user_service};
-use crate::models::user::User;
+use crate::models::user::{User, NewUser};
+
 #[get("/")]
 async fn list_users(state: web::Data<AppState>) -> HttpResponse {
     match state.user_service.get_users().await {
@@ -8,7 +9,16 @@ async fn list_users(state: web::Data<AppState>) -> HttpResponse {
         Err(e) => HttpResponse::BadRequest().body(e.to_string()),
     }
 }
+#[post("/")]
+async fn add_user(state: web::Data<AppState>, req: web::Json<NewUser>) -> HttpResponse {
+    let new_user = req.into_inner();
+    match state.user_service.create_user(new_user).await {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(e) => HttpResponse::BadRequest().body(e.to_string())
+    }
+}
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(list_users);
+    cfg.service(add_user);
 }
