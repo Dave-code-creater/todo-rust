0dc36be890016342900b56a05e7c735f5f36b08c
Dave-code-creater <dat981dat@gmail.com>
2025-11-16 17:10:04 -0500

    Add DTO for properly format output and input
    Signed-off-by: Dave-code-creater <dat981dat@gmail.com>

---
+++ src/dto/user_response.rs
@@ -0,0 +1,21 @@
+use serde::Serialize;
+use derive_more::From;
+
+use crate::models::user::User;
+
+#[derive(Debug, Serialize)]
+pub struct UserResponse {
+    pub id: String,
+    pub username: String,
+    pub email: String,
+}
+
+impl From<User> for UserResponse {
+    fn from(user: User) -> Self {
+        Self {
+            id: user.id.expect("REASON").to_hex(),
+            username: user.username,
+            email: user.email,
+        }
+    }
+}
